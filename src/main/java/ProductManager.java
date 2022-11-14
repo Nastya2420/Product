74883b33e5031abebe796b8e8c1165c940ad5e87
Anastasia <a.grebenkina93@gmail.com>
2022-11-14 21:56:57 +0300

    Java

--- src/main/java/ProductManager.java
+++ src/main/java/ProductManager.java
@@ -10,7 +10,7 @@ public class ProductManager {
     }
 
     public Product[] searchBy(String text) {
-        Product[] result = new Product[0]; // тут будем хранить подошедшие запросу продукты
+        Product[] result = new Product[0];
         for (Product product : repository.findAll()) {
             if (matches(product, text)) {
                 Product[] tmp = new Product[result.length + 1];
@@ -21,14 +21,12 @@ public class ProductManager {
         return result;
     }
 
-    // метод определения соответствия товара product запросу search
+
     public boolean matches(Product product, String search) {
         if (product.getName().contains(search)) {
             return true;
         } else {
             return false;
         }
-        // или в одну строку:
-        // return product.getName().contains(search);
     }
 }
